a279c4233567cd3597153e8255dda0d75a885df2
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-10-02 14:54:32 +0200

    Vers 1 update

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ require_once 'classes/magazzino.php';
 $Tv2 = new Tv('Lg','70','800');
 $Tv2->setPosition('265L');
 var_dump($Tv2);
+
+$Tv3 = new Tv('Sony','35','600');
+$Tv3->setPosition('201G');
+var_dump($Tv3);
+
  ?>
